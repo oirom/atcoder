dc777484eac1a6bf33ef70b8613ce196a9bf70e8
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-03-25 10:53:10 +0000

    first commit

---
+++ AtCoder_Begginners_Selection/code_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    if ((a*b)%2 == 0)
+        cout << "Even\n";
+    else
+        cout << "Odd\n";
+    
+    return 0;
+}
