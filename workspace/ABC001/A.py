d1f692a7e8359406f473db1fb0e1fe3de3f2a033
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-07-26 18:25:37 +0000

    add .gitignore

--- workspace/ABC001/A.py
+++
@@ -1,6 +0,0 @@
-def main():
-    h1, h2 = (int(x) for x in input().split())
-    print(h1-h2)
-
-if __name__ == '__main__':
-    main()
--- workspace/ABC036/A.py
+++
@@ -1,5 +0,0 @@
-import math
-
-a, b = input().split()
-
-print(math.ceil((int(b)/int(a))))
