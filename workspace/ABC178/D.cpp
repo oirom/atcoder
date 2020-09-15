9d6a7a810714b315d2512192fdd210f5e318b8c4
oirom <yamauchi.morio853@mail.kyutech.jp>
2020-09-15 14:50:19 +0000

    daily streak

--- workspace/ABC178/D.cpp
+++ workspace/ABC178/D.cpp
@@ -13,12 +13,23 @@ using VPLD = vector<PLD>;
 
 const int INF = numeric_limits<int>::max();
 const unsigned long long int ULLINF = numeric_limits<unsigned long long int>::max();
+const long long mod = 1000000007;
 
 int main() {
     
-    int x;
-    cin >> x;
-    if (x==1) cout << 0 << endl;
-    else cout << 1 << endl;
+    LL s;
+    cin >> s;
+
+    VLL v(2001,0);
+    v[0] = 1;   // 空なる数列も許容するとする
+    v[1] = 0;
+    v[2] = 0;
+    v[3] = 1;
+
+    for (LL i=4; i<=2000; i++) {
+        v[i] = (v[i-1] + v[i-3])%mod;
+    }
+
+    cout << v[s] << endl;
 
 }
