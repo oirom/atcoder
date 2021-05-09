1e73188c465c7d9dc3052699043552fa07571365
oirom <yamauchi.morio853@mail.kyutech.jp>
2021-05-09 09:53:11 +0000

    attend ABC200

--- workspace/typical90/02.cpp
+++ workspace/typical90/02.cpp
@@ -2,35 +2,34 @@
 using namespace std;
 
 bool hantei(string s) {
-    int cnt = 0;
-    for (int i = 0; i < (int)s.size(); i++) {
-        if (s[i] == '(') cnt++;
-        if (s[i] == ')') cnt--;
-        if (cnt < 0) return false;
-    }
-    if (cnt == 0) return true;
-    return false;
+  int cnt = 0;
+  for (int i = 0; i < (int)s.size(); i++) {
+    if (s[i] == '(') cnt++;
+    if (s[i] == ')') cnt--;
+    if (cnt < 0) return false;
+  }
+  if (cnt == 0) return true;
+  return false;
 }
 
 int main() {
+  int N;
+  cin >> N;
 
-    int N;
-    cin >> N;
-    
-    for (int i = 0; i < (1 << N); i++) {
-        string str = "";
-        //cout << bitset<4>(i) << ": ";
-        //for (int bit = N-1; bit >= 0; bit--) {
-        for (int bit = 0; bit < N; bit++) {
-            if ((1 & (i >> bit)) == 0) {
-                str += "(";
-            } else {
-                str += ")";
-            }
-        }
-
-        if (hantei(str)) cout << str << endl;
+  for (int i = 0; i < (1 << N); i++) {
+    string str = "";
+    // cout << bitset<4>(i) << ": ";
+    // for (int bit = N-1; bit >= 0; bit--) {
+    for (int bit = 0; bit < N; bit++) {
+      if ((1 & (i >> bit)) == 0) {
+        str += "(";
+      } else {
+        str += ")";
+      }
     }
 
-    return 0;
+    if (hantei(str)) cout << str << endl;
+  }
+
+  return 0;
 }
