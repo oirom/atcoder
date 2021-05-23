1a9e56846a176e20cb71a778f86ef8243d49cbbf
oirom <yamauchi.morio853@mail.kyutech.jp>
2021-05-23 08:42:54 +0000

    daily streak

--- workspace/ABC047/B.cpp
+++ workspace/ABC047/B.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#define REP(i,n) for (int i=0; i<(n); ++i)
-#define REPR(i,n,m) for (int i=(n); i>=(m); --i)
+#define rep(i,n) for (int i=0; i<(n); ++i)
+#define repr(i,n,m) for (int i=(n); i>=(m); --i)
 using namespace std;
 using LL = long long;
 using LD = long double;
@@ -13,14 +13,30 @@ using VPLD = vector<PLD>;
 const int INF = numeric_limits<int>::max();
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
 
-    int w, h, n; cin >> w >> h >> n;
-    vector<int> x(n), y(n), a(n); REP(i,n) cin >> x[i] >> y[i] >> a[i];
+    int w, h, n;
+    cin >> w >> h >> n;
+    vector<int> x(n), y(n), a(n);
+    rep(i,n) cin >> x[i] >> y[i] >> a[i];
+    //rep(i,n) cin >> y[i];
+    //rep(i,n) cin >> a[i];
 
+    int left = 0, right = w, top = h, bottom = 0;
+
+    rep(i,n) {
+        if (a[i] == 1) left = max(left, x[i]);
+        if (a[i] == 2) right = min(right, x[i]);
+        if (a[i] == 3) bottom = max(bottom, y[i]);
+        if (a[i] == 4) top = min(top, y[i]); 
+    }
+
+    //printf("L:%d, R:%d, T:%d, B:%d\n", left, right, top, bottom);
     
-    
+    if (right > left && top > bottom) {
+        cout << (right - left) * (top - bottom) << endl;
+    } else {
+        cout << 0 << endl;
+    }
 
     return 0;
 }
