5a396865d53665275bf916ec8cb0fe6c3cea6ec8
oirom <yamauchi.morio853@mail.kyutech.jp>
2021-06-19 00:15:59 +0900

    daily streak

--- workspace/AGC022/A.cpp
+++ workspace/AGC022/A.cpp
@@ -5,39 +5,35 @@ int main() {
 
   string s;
   cin >> s;
-  vector<int> v(26,0);
-  
-  if (s == "zyxwvutsrqponmlkjihgfedcba") {cout << -1 << endl; return 0;}
+  vector<bool> F(128);
 
-  for (int i = 0; i < (int)s.size(); i++) {
-    v[(int)(s[i] - '0') - 49] += 1;
-  }
+  int n = (int)s.size();
 
-  if ((int)s.size() < 26) {
-    cout << s;
-    int i = 0;
-    while(v[i] == 1) i++;
-    cout << char('a' + i) << endl;
-    return 0;
-  }
+  // flag to judge that s is zyxwv...dcba or not
+  int A = 0;
+  for (int i = 0; i < n-1; i++) A |= (s[i] < s[i+1]);
 
-  priority_queue<char, vector<char>, greater<char>> alp;
-
-  int i;
-  for (i = 25; i > 0; i--) {
-    if (s[i-1] < s[i]) break;
-    else alp.push(s[i]);
-  }
-  
-  if (alp.empty()) cout << s.substr(0, i-1) << s[i] << endl;
-
-  else {
-    while (!alp.empty()) {
-      if (s[i-1] < alp.top()) break;
-      alp.pop();
+  if ( n == 26) {
+    if (!A) cout << -1 << endl;
+    else {
+      string t = s;
+      next_permutation(s.begin(), s.end());
+      for (int i = 0; i < n; i++) {
+        cout << s[i];
+        if (s[i] != t[i]) break;
+      }
+      cout << endl;
     }
-    cout << s.substr(0, i-1) << alp.top() << endl;
+  } else {
+    for (int i = 0; i < n; i++) F[s[i]] = 1;
+    for (char c = 'a'; c <= 'z'; c++) {
+      if (!F[c]) {
+        cout << s;
+        cout << c << endl;
+        break;
+      }
+    }
   }
-
+  
   return 0;
 }
